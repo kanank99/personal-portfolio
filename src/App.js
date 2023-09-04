76141712a50c64be55d5621f03473e063a91a916
Kanan Kontorovsky <kanankontorovsky@hotmail.com>
2023-09-04 12:04:40 -0400

    built custom cursor / small style changes to Hero

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import "./App.css";
 import Hero from "./components/Hero";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const cursor = document.getElementById("cursor");
+    document.documentElement.addEventListener("mousemove", (evt) => {
+      const mouseX = evt.clientX + "px";
+      const mouseY = evt.clientY + "px";
+      const root = document.documentElement;
+      cursor.style.setProperty("display", "inline-block");
+      setTimeout(() => {
+        root.style.setProperty("--cursorX", mouseX);
+        root.style.setProperty("--cursorY", mouseY);
+      }, 160);
+    });
+
+    document.documentElement.addEventListener("mouseleave", (evt) => {
+      cursor.style.setProperty("display", "none");
+    });
+  }, []);
+
   return (
     <div className="App">
+      <span id="cursor">
+        <svg height="30" width="30">
+          <circle cx="15" cy="15" r="12" stroke-width="0" fill="#f7f8fa" />
+        </svg>
+      </span>
       <Hero />
     </div>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,24 +5,26 @@ function Hero() {
     <div className="gradient-wrapper">
       <div className="bg-grid h-[100svh] w-full flex items-center justify-center">
         <div className="hero-container text-left w-[824px]">
-          <div className="hero-text flex flex-col gap-4">
+          <div className="hero-text flex flex-col gap-2 lg:gap-4 p-8">
             <h1 className="text-base tracking-tight text-[#38bcf8]">
-              Hi, my name is,
+              Hi, my name is
             </h1>
-            <h1 className="text-7xl font-extrabold text-white tracking-wide mt-2">
+            <h1 className="text-[2rem] md:text-6xl leading-10 lg:text-7xl font-extrabold text-white tracking-wide mt-2 whitespace-nowrap">
               Kanan Kontorovsky.
             </h1>
-            <h2 className="text-7xl font-extrabold text-white/60">
+            <h2 className="text-4xl md:text-6xl lg:text-7xl font-extrabold text-white/60 whitespace-nowrap">
               I build for everyone
             </h2>
-            <p className="text-lg font-medium w-[90%] text-[#f7f7f7] mt-6">
-              I'm a software engineer specializing in building (and occasionally
-              designing) exceptional digital experiences. Currently, I'm focused
-              on building accessible, human-centered products at{" "}
+            <p className="text-lg lg:w-[90%] text-[#f7f7f7] mt-6 font-light leading-6">
+              I specialize in
+              <span className="text-3"> React Front-End Development</span>,
+              where I bring web interfaces to life with creativity and
+              precision. My focus is on creating immersive, user-centric
+              experiences that showcase the full potential of React.
             </p>
-            <div class="frame">
-              <a className="anchor" href="https://www.flowchef.co/">
-                <div class="button font-bold text-[#38bcf8]">Get In Touch</div>
+            <div class="frame mt-5">
+              <a className="anchor cursor-none" href="https://www.flowchef.co/">
+                <div class="button font-bold">Get In Touch</div>
                 <div class="button_bg-grad"></div>
               </a>
             </div>
